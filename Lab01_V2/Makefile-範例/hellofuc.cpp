a14c1dba179f7f993ff806ce009d3abb71ffc315
bradly0cjw <57744542+bradly0cjw@users.noreply.github.com>
2025-09-26 13:33:00 +0800

    add new HW1

---
+++ Lab01_V2/Makefile-範例/hellofuc.cpp
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void hellofunc(){
+    printf("hellofunc!\n");
+}
---
+++ Lab01_V2/Makefile-範例/hellomake.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+void hellofunc();
+int main(){
+    hellofunc();
+    cout<<"hello make!"<<endl;
+    return 0;
+}
